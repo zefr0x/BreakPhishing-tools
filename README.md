841f7c7c03a05299cc599fbccb87d8ca99ef6783
zer0-x <zer0-x.7ty50@aleeas.com>
2021-01-09 07:11:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Break-Phishing tools
+
+## Requirements
+
+* [python >= 3.6](https://www.python.org/)
+
+* [python requests library](https://github.com/psf/requests)
+  
+### if the script uses tor
+
+* [tor service running in background](http://3gldbgtv5e4god56.onion/onion-services/setup/install/index.html) or you can just open [tor browser](https://www.torproject.org/)
+
+* Names and passwords lists in `./lists` directory (**no need** they are included by default)
+
+## notes
+
+*Read the comments in the scripts to know more.*
+
+*Use it for legal work only.*
---
+++ simple fake login with tor.py
@@ -0,0 +1,74 @@
+import requests
+import os
+import random
+import string
+import time
+
+#url where the username and the password will be sent (include http or https)
+url = 'http://???.com/index.php'
+
+#where the username and password will be sent
+send_name_to_value= 'username'
+send_pass_to_value= 'password'
+
+#how meny fake user you want to send??
+repetition = 1000
+
+
+start_time = time.time()
+
+def get_tor_session():
+    session = requests.session()
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+
+
+    print("tor ip:" + session.get("http://httpbin.org/ip").text)
+    print("real ip:" +requests.get("http://httpbin.org/ip").text)
+
+    return session
+
+#start a tor session
+tor_session = get_tor_session()
+
+
+
+chars = string.digits + string.ascii_letters + '!@#$%^&*()'
+
+
+#opining names and passwords lists
+names = open('./lists/names.list', 'r').read().splitlines()
+passwords = open('./lists/fierfox-pass.list', 'r').read().splitlines()
+
+
+
+for i in range(repetition):
+    name_extra_len = random.randint(0, 4)
+    pass_extra_len = random.randint(3, 9)
+
+    name_extra = ''.join(random.sample(string.digits, name_extra_len))
+    pass_extra = ''.join(random.sample(chars, pass_extra_len))
+
+    if random.randint(0, 2) == 0:
+        secend_name = '_' + random.choice(names).lower()
+    else:
+        secend_name = ''
+
+    username = random.choice(names).lower() + secend_name + name_extra
+    password = random.choice(passwords) + pass_extra
+
+
+
+
+    try:
+        tor_session.post(url, allow_redirects=False, data={
+        send_name_to_value: username,
+        send_pass_to_value: password
+    })
+
+        print(f'{i}) sent      username--> {username}      password--> {password} \n') 
+
+
+    except KeyboardInterrupt:
+        print(f"\n-     {i} fake user was sent in {round(time.time() - start_time, 2)}s")
+        exit()
